0a9c95f31caab73e33aa4f9212ad1ff937651946
Supriya Mahalik <supriya.mahalik@invincix.com>
2023-09-25 21:35:49 +0530

    initial commit

---
+++ src/context/cart_context.js
@@ -0,0 +1,78 @@
+import { createContext, useContext, useReducer, useEffect } from "react";
+import reducer from "../reducer/cartReducer";
+
+const CartContext = createContext();
+
+const getLocalCartData = () => {
+  let localCartData = localStorage.getItem("shopCart");
+  if (localCartData === []) {
+    return [];
+  } else {
+    return JSON.parse(localCartData);
+  }
+};
+
+const initialState = {
+  // cart: [],
+  cart: getLocalCartData(),
+  total_item: "",
+  total_amount: "",
+  shipping_fee: 50000,
+};
+
+const CartProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const addToCart = (id, color, amount, product) => {
+    dispatch({ type: "ADD_TO_CART", payload: { id, color, amount, product } });
+  };
+
+  const removeItem = (id) => {
+    dispatch({ type: "REMOVE_ITEM", payload: id });
+  };
+
+  //Incerement and decrement the product
+
+  const setDecrement = (id) => {
+    dispatch({ type: "SET_DECREMENT", payload: id});
+  }
+
+  const setIncrement = (id) => {
+    dispatch({ type: "SET_INCREMENT", payload: id});
+  }
+
+   // to clear the cart
+   const clearCart = () => {
+    dispatch({ type: "CLEAR_CART" });
+  };
+
+  // to add the data in localStorage
+  // get vs set
+
+  useEffect(() => {
+    // dispatch({ type: "CART_TOTAL_ITEM" });
+    // dispatch({ type: "CART_TOTAL_PRICE" });
+    dispatch({ type: "CART_ITEM_PRICE_TOTAL" });
+    
+    localStorage.setItem("shopCart", JSON.stringify(state.cart));
+  }, [state.cart]);
+
+  return (
+    <CartContext.Provider value={{
+       ...state, 
+       addToCart, 
+       removeItem,
+       clearCart,
+       setDecrement,
+       setIncrement
+       }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+const useCartContext = () => {
+  return useContext(CartContext);
+};
+
+export { CartProvider, useCartContext };
